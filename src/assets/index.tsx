5d6aacfd9098226ea8180bb461ca5b7312d87cf8
Phajsi <phajsi@gmail.com>
2023-05-06 22:48:08 +0200

    Add react router ++

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -3,3 +3,4 @@ export { default as ExhaustionColSVG } from "./ExhaustionColSVG";
 export { default as CynicismColSVG } from "./CynicismColSVG";
 export { default as InefficacyColSVG } from "./InefficacyColSVG";
 export { default as BurnoutLogoSVG } from "./BurnoutLogoSVG";
+export { default as BurnoutLogoIconSVG } from "./BurnoutLogoIconSVG";
