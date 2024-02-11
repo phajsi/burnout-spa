1cb540bdeb5e8a655c2b0950dab93e60fa565aa5
Phajsi <phajsi@gmail.com>
2024-02-11 11:49:26 +0100

    Add scene1 page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App = () => {
         <Route path="/" element={<Home />}></Route>
         <Route index element={<Home />}></Route>
         <Route path="burnout-in-tech" element={<Burnout />}></Route>
+        <Route path="burnout-in-tech_scene-1"></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Scene1.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Scene1 = () => {
+  return <div></div>;
+};
+
+export default Scene1;
