f62184d44f74142dc9b7a74ae6f5fc2a0eaf0fdf
phajsi <23497392+phajsi@users.noreply.github.com>
2026-07-31 18:19:14 +0200

    Remove tailwind classes from home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
 import Burnout from "./pages/Burnout"
 import Home from "./pages/Home"
 import SceneIntro from "./pages/SceneIntro"
 import Scene1 from "./pages/Scene1"
 
+const BurnoutLayout = () => (
+  <div className="min-h-screen bg-brand-teal/95 text-white font-sans">
+    <Outlet />
+  </div>
+)
+
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-brand-teal/95 text-white font-sans">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route index element={<Home />}></Route>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route element={<BurnoutLayout />}>
           <Route path="burnout-in-tech" element={<Burnout />}></Route>
           <Route path="burnout-in-tech/intro" element={<SceneIntro />}></Route>
           <Route path="burnout-in-tech/scene-1" element={<Scene1 />}></Route>
-        </Routes>
-      </div>
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
